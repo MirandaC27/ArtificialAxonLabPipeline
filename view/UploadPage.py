4896d43238f50680ad4f36637a11d037d073bfee
jonathanDargakis <jmdargakis@loyola.edu>
2026-02-06 23:32:16 -0500

    Raw and Cleaned folder tracks upload to JSON #5

--- view/UploadPage.py
+++ view/UploadPage.py
@@ -1,33 +1,45 @@
 import tkinter as tk
 from tkinter import filedialog
+from pathlib import Path
 import json
 
-JSON_PATH = "file_paths.json"
+selected_folders = []
 
-def upload_files():
-    file_paths = filedialog.askopenfilenames(
-        title="Select files"
-    )
+def add_folder():
+    folder = filedialog.askdirectory(title="Select a folder")
+    if folder:
+        selected_folders.append(folder)    
+        status_label.config(
+            text="Selected folders:\n" + "\n".join(selected_folders)
+        )
+        save_folders()
 
-    if not file_paths:
-        return
+def save_folders():
+    tracks = set()
+    tracks1 = set()
+
+    for folder in selected_folders:
+        root = Path(folder)
+
+        folder_name = root.name.upper()
+        if folder_name.endswith("RAW"):
+            tracks.add(str(root))
+        elif folder_name.endswith("CLEANED"):
+            tracks1.add(str(root))
 
     data = {
-        "files": list(file_paths)
+        "TRACKS": sorted(tracks),
+        "TRACKS1": sorted(tracks1)
     }
 
-    # Save paths to JSON
-    with open(JSON_PATH, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
-
-    status_label.config(text=f"{len(file_paths)} file paths saved")
-
+    with open("folder_paths.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4) 
 
 window = tk.Tk()
 window.title("File Uploader")
 window.geometry("600x300")
 
-tk.Button(window, text="Upload File Paths", command=upload_files).pack(pady=120)
+tk.Button(window, text="Add Folder", command=add_folder).pack(pady=10)
 
 status_label = tk.Label(window, text="")
 status_label.pack(pady=5)
