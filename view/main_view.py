03c4fd5a3f757351547cd6ebbee586d92332f925
jonathanDargakis <jmdargakis@loyola.edu>
2026-04-01 12:28:00 -0400

    added next/back buttons and adjusted frame size for mask #72

--- view/main_view.py
+++ view/main_view.py
@@ -43,6 +43,11 @@ class App(tk.Tk):
         return self.pages.get(name)
     
     def show_page(self, name):
+        if name == "Masking Settings":
+            self.geometry("700x600")
+        else:
+            self.geometry("700x500")
+
         page = self.pages[name]
         page.tkraise()
 
