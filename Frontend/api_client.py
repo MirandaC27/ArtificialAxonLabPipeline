164952592eef6a550de408485f73c0b3031f6377
jonathanDargakis <jmdargakis@loyola.edu>
2026-06-17 18:30:30 -0400

    fixed port

--- Frontend/api_client.py
+++ Frontend/api_client.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-BASE_URL = os.getenv("AXONLAB_API_URL", "http://localhost:8001").rstrip("/")
+BASE_URL = os.getenv("AXONLAB_API_URL", "http://localhost:8000").rstrip("/")
 
 #---------------------------------------------------
 def add_numbers(a, b):
